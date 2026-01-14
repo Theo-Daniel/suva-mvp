dcc384dfce12c38ce78675e572a62b1ea63f8e33
Theo <dani91020@gmail.com>
2026-01-14 01:53:14 +0000

    Fix router nesting and paths for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing.jsx";
 import WalletPage from "./pages/Wallet.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/wallet" element={<WalletPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/wallet" element={<WalletPage />} />
+    </Routes>
   );
 }
